c689ab95c74ba1fa417ef9b46ab3d0aaa659c5e9
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-01-23 23:22:09 +0200

    LSN-2: show photo by clicking on Prev button

--- source/basic-redux/bus/gallery/actions.js
+++ source/basic-redux/bus/gallery/actions.js
@@ -1,5 +1,9 @@
 // Types
-import { SHOW_NEXT_PHOTO, SHOW_SELECTED_PHOTO } from "./types";
+import { SHOW_NEXT_PHOTO, SHOW_SELECTED_PHOTO, SHOW_PREV_PHOTO } from "./types";
+
+export const showPrevPhoto = {
+    type: SHOW_PREV_PHOTO,
+};
 
 export const showNextPhoto = {
     type: SHOW_NEXT_PHOTO,
