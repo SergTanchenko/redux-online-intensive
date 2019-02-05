e8a29f1859b2a493f12f2ce052a835b053d334cc
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-02-06 00:46:50 +0200

    LSN-5-2: splitted routers for authenticated and non-authenticated users

--- source/components/Forms/Signup.js
+++ source/components/Forms/Signup.js
@@ -1,11 +1,11 @@
 // Core
-import React, { Component } from 'react';
-import { Formik, Form, Field } from 'formik';
-import cx from 'classnames';
+import React, { Component } from "react";
+import { Formik, Form, Field } from "formik";
+import cx from "classnames";
 
 // Instruments
-import Styles from './styles.m.css';
-import { signup } from '../../bus/forms/shapes';
+import Styles from "./styles.m.css";
+import { signup } from "../../bus/forms/shapes";
 
 export default class SignupForm extends Component {
     static defaultProps = {
@@ -29,28 +29,39 @@ export default class SignupForm extends Component {
                 render = { (props) => {
                     const { isValid, touched, errors } = props;
 
-                    const centeredWrapperStyle = cx(Styles.wrapper, Styles.centered, {
-                        [Styles.disabledInput]: isFetching,
-                    });
+                    const centeredWrapperStyle = cx(
+                        Styles.wrapper,
+                        Styles.centered,
+                        {
+                            [Styles.disabledInput]: isFetching,
+                        }
+                    );
                     const firstNameStyle = cx({
-                        [Styles.invalidInput]: !isValid && touched.firstName && errors.firstName,
+                        [Styles.invalidInput]:
+                            !isValid && touched.firstName && errors.firstName,
                     });
                     const lastNameStyle = cx({
-                        [Styles.invalidInput]: !isValid && touched.lastName && errors.lastName,
+                        [Styles.invalidInput]:
+                            !isValid && touched.lastName && errors.lastName,
                     });
                     const emailStyle = cx({
-                        [Styles.invalidInput]: !isValid && touched.email && errors.email,
+                        [Styles.invalidInput]:
+                            !isValid && touched.email && errors.email,
                     });
                     const passwordStyle = cx({
-                        [Styles.invalidInput]: !isValid && touched.password && errors.password,
+                        [Styles.invalidInput]:
+                            !isValid && touched.password && errors.password,
                     });
                     const inviteStyle = cx({
-                        [Styles.invalidInput]: !isValid && touched.invite && errors.invite,
+                        [Styles.invalidInput]:
+                            !isValid && touched.invite && errors.invite,
                     });
                     const buttonStyle = cx(Styles.signupSubmit, {
                         [Styles.disabledButton]: isFetching,
                     });
-                    const buttonMessage = isFetching ? 'Загрузка...' : 'Создать аккаунт ✓';
+                    const buttonMessage = isFetching
+                        ? "Loading..."
+                        : "Create account ✓";
 
                     return (
                         <Form className = { Styles.form }>
@@ -60,38 +71,41 @@ export default class SignupForm extends Component {
                                         className = { firstNameStyle }
                                         disabled = { isFetching }
                                         name = 'firstName'
-                                        placeholder = 'Имя'
+                                        placeholder = 'First Name'
                                         type = 'text'
                                     />
                                     <Field
                                         className = { lastNameStyle }
                                         disabled = { isFetching }
                                         name = 'lastName'
-                                        placeholder = 'Фамилия'
+                                        placeholder = 'Last Name'
                                         type = 'text'
                                     />
                                     <Field
                                         className = { emailStyle }
                                         disabled = { isFetching }
                                         name = 'email'
-                                        placeholder = 'Почта'
+                                        placeholder = 'Email'
                                         type = 'email'
                                     />
                                     <Field
                                         className = { passwordStyle }
                                         disabled = { isFetching }
                                         name = 'password'
-                                        placeholder = 'Пароль'
+                                        placeholder = 'Password'
                                         type = 'password'
                                     />
                                     <Field
                                         className = { inviteStyle }
                                         disabled = { isFetching }
                                         name = 'invite'
-                                        placeholder = 'Секретное слово'
+                                        placeholder = 'Secret word'
                                         type = 'password'
                                     />
-                                    <button className = { buttonStyle } disabled = { isFetching } type = 'submit'>
+                                    <button
+                                        className = { buttonStyle }
+                                        disabled = { isFetching }
+                                        type = 'submit'>
                                         {buttonMessage}
                                     </button>
                                 </div>
--- source/components/Nav/index.js
+++ source/components/Nav/index.js
@@ -1,20 +1,26 @@
 // Core
-import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
-import cx from 'classnames';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import cx from "classnames";
 
 // Instruments
-import Styles from './styles.m.css';
-import { book } from '../../navigation/book';
-import { mockedProfile } from '../../instruments/mockedData';
+import Styles from "./styles.m.css";
+import { book } from "../../navigation/book";
+import { mockedProfile } from "../../instruments/mockedData";
 
-@withRouter
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.get("isAuthenticated"),
+    };
+};
+
+@connect(mapStateToProps)
 export default class Nav extends Component {
     static defaultProps = {
         // State
-        profile:         mockedProfile,
-        isAuthenticated: true,
-        isOnline:        false,
+        profile:  mockedProfile,
+        isOnline: false,
 
         // Actions
         logoutAsync: () => {},
@@ -23,32 +29,32 @@ export default class Nav extends Component {
     _getNav = () => {
         const { isAuthenticated, profile } = this.props;
 
-        return isAuthenticated ?
+        return isAuthenticated ? (
             <>
                 <div>
                     <NavLink activeClassName = { Styles.active } to = { book.profile }>
-                        <img src = { profile.get('avatar') } />
-                        {profile.get('firstName')}
+                        <img src = { profile.get("avatar") } />
+                        {profile.get("firstName")}
                     </NavLink>
                     <NavLink activeClassName = { Styles.active } to = { book.feed }>
-                        Стена
+                        Wall
                     </NavLink>
                 </div>
-                <button onClick = { this._logout }>Выйти</button>
+                <button onClick = { this._logout }>Sign out</button>
             </>
-            :
+        ) : (
             <>
                 <div>
                     <NavLink activeClassName = { Styles.active } to = { book.login }>
-                        Войти
+                        Sign in
                     </NavLink>
                     <NavLink activeClassName = { Styles.active } to = { book.signUp }>
-                        Создать аккаунт
+                        Create account
                     </NavLink>
                 </div>
-                <button className = { Styles.hidden }>Выйти</button>
+                <button className = { Styles.hidden }>Sign out</button>
             </>
-        ;
+        );
     };
 
     _logout = () => {
@@ -67,7 +73,7 @@ export default class Nav extends Component {
         return (
             <section className = { Styles.navigation }>
                 <div className = { statusStyle }>
-                    <div>{isOnline ? 'Онлайн' : 'Офлайн'}</div>
+                    <div>{isOnline ? "Онлайн" : "Офлайн"}</div>
                     <span />
                 </div>
                 {navigation}
