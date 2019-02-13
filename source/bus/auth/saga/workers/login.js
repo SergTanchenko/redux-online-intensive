4574aea10264cf05f605d066ef9b4989a03a9542
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-02-13 16:38:59 +0200

    LSN-6-4: fixed saving of token

--- source/bus/auth/saga/workers/login.js
+++ source/bus/auth/saga/workers/login.js
@@ -17,10 +17,10 @@ export function* login ({ payload: credentials }) {
             throw new Error(message);
         }
 
-        if (credentials.remember) {
-            yield apply(localStorage, localStorage.setItem, ["remember", true]);
-        }
-
+        yield apply(localStorage, localStorage.setItem, [
+            "token",
+            profile.token
+        ]);
         yield put(profileActions.fillProfile(profile));
         yield put(authActions.authenticate());
     } catch (error) {
