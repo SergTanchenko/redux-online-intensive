a9d707aeaee22428745952bedabc93212f17bf72
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-02-17 20:24:42 +0200

    LSN-7: handle like/unlike posts

--- source/bus/socket/actions.js
+++ source/bus/socket/actions.js
@@ -11,11 +11,27 @@ export const socketActions = {
             dispatch(uiActions.setOfflineState());
         });
     },
-    listenPosts: () => (dispatch) => {
+    listenPosts: () => (dispatch, getState) => {
         socket.on("create", (event) => {
             const { data: post } = JSON.parse(event);
 
             dispatch(postActions.createPost(post));
+        });
+        socket.on("like", (event) => {
+            const {
+                data: { userId, postId },
+                meta,
+            } = JSON.parse(event);
+
+            if (meta.action === "like") {
+                const liker = getState()
+                    .users.find((user) => user.get("id") === userId)
+                    .delete("avatar");
+
+                dispatch(postActions.likePost(liker, postId));
+            } else {
+                dispatch(postActions.unlikePost(postId));
+            }
         });
     },
 };
