f7e22630edcd10cd2c040cf7cfa94427d02f7617
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-02-24 17:53:14 +0200

    LSN-10: added auth tests

---
+++ source/bus/auth/__tests__/actions.js
@@ -0,0 +1,52 @@
+import { types } from "./../types";
+import { authActions } from "../actions";
+
+describe("auth actions: ", () => {
+    test("authenticate", () => {
+        expect(authActions.authenticate()).toEqual({
+            type: types.AUTHENTICATE,
+        });
+    });
+    test("initialize", () => {
+        expect(authActions.initialize()).toEqual({
+            type: types.INITIALIZE,
+        });
+    });
+    test("logout", () => {
+        expect(authActions.logout()).toEqual({
+            type: types.LOGOUT,
+        });
+    });
+
+    test("signupAsync", () => {
+        expect(authActions.signupAsync(__.userData)).toEqual({
+            type:    types.SIGNUP_ASYNC,
+            payload: __.userData,
+        });
+    });
+
+    test("loginAsync", () => {
+        expect(authActions.loginAsync(__.credentials)).toEqual({
+            type:    types.LOGIN_ASYNC,
+            payload: __.credentials,
+        });
+    });
+
+    test("authenticateAsync", () => {
+        expect(authActions.authenticateAsync()).toEqual({
+            type: types.AUTHENTICATE_ASYNC,
+        });
+    });
+
+    test("initializeAsync", () => {
+        expect(authActions.initializeAsync()).toEqual({
+            type: types.INITIALIZE_ASYNC,
+        });
+    });
+
+    test("logoutAsync", () => {
+        expect(authActions.logoutAsync()).toEqual({
+            type: types.LOGOUT_ASYNC,
+        });
+    });
+});
