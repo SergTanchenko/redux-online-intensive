251768c12a51d01bcd117daa9b91e059cc74e0b8
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-02-24 22:00:48 +0200

    LSN-10: cover users domen with tests

--- jest/scripts/setupFiles.js
+++ jest/scripts/setupFiles.js
@@ -1,23 +1,38 @@
 // Mocks
-import { LocalStorage } from './mocks/localStorage';
-import { fetch } from './mocks/fetch';
+import { LocalStorage } from "./mocks/localStorage";
+import { fetch } from "./mocks/fetch";
 
-const successMesasge = 'TEST_SUCCESS_MESSAGE.';
-const errorMessage = 'TEST_ERROR_MESSAGE.';
-const token = 'TEST_TOKEN';
+const successMesasge = "TEST_SUCCESS_MESSAGE.";
+const errorMessage = "TEST_ERROR_MESSAGE.";
+const token = "TEST_TOKEN";
 const error = new Error(errorMessage);
 
 const userProfile = {
-    id:        'TEST_ID',
-    avatar:    'TEST_AVATAR',
-    firstName: 'Walter',
-    lastName:  'White',
+    id:        "TEST_ID",
+    avatar:    "TEST_AVATAR",
+    firstName: "Walter",
+    lastName:  "White",
     token,
 };
 
+const users = [
+    {
+        id:        "111",
+        firstName: "Vincenzo",
+        lastName:  "Smith",
+        avatar:    "some.jpg",
+    },
+    {
+        id:        "222",
+        firstName: "Danika",
+        lastName:  "Volkman",
+        avatar:    "some.jpg",
+    }
+];
+
 const credentials = {
-    email:    'test@email.com',
-    password: '1111',
+    email:    "test@email.com",
+    password: "1111",
     remember: true,
 };
 
@@ -45,10 +60,11 @@ const fetchResponseFail400 = {
     json:   jest.fn(() => Promise.resolve(responseDataFail)),
 };
 
-const url = 'https://www.url.com';
+const url = "https://www.url.com";
 
 global.__ = {
     userProfile,
+    users,
     errorMessage,
     token,
     error,
--- source/bus/auth/__tests__/reducer.js
+++ source/bus/auth/__tests__/reducer.js
@@ -9,23 +9,23 @@ const initialState = Map({
 
 describe("test auth reducer: ", () => {
     test("should return initialState by default", () => {
-        expect(authReducer(undefined, {})).toEqual(initialState);
+        expect(authReducer(void 0, {})).toEqual(initialState);
     });
 
     test("should handle AUTHENTICATE action", () => {
-        expect(authReducer(undefined, authActions.authenticate())).toEqual(
+        expect(authReducer(void 0, authActions.authenticate())).toEqual(
             initialState.set("isAuthenticated", true)
         );
     });
 
     test("should handle INITIALIZE action", () => {
-        expect(authReducer(undefined, authActions.initialize())).toEqual(
+        expect(authReducer(void 0, authActions.initialize())).toEqual(
             initialState.set("isInitialized", true)
         );
     });
 
     test("should handle LOGOUT action", () => {
-        expect(authReducer(undefined, authActions.logout())).toEqual(
+        expect(authReducer(void 0, authActions.logout())).toEqual(
             initialState.set("isAuthenticated", false)
         );
     });
